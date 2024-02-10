c5344f50058a5038f0d521e836168e143555dde1
Arda Batuhan Demir <arbade@Ardas-MacBook-Pro.local>
2024-02-10 22:43:47 +0300

    revert changes

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const core = require('@actions/core');
 const { readFileSync } = require('fs');
-const { execSync } = require('child_process');
 const axios = require('axios');
 
 async function run() {
     try {
         const slackWebhookURL = core.getInput('slack-webhook-url');
-        const changelog = getChangelogFromEvent();
-        console.log('Changelog:', changelog);
+        const releaseNotes = getReleaseNotesFromEvent();
+        console.log('Release Notes:', releaseNotes);
 
-        const { changes, colorHex } = processChangelog(changelog.body);
+        if (!releaseNotes) {
+            throw new Error('Release notes not found');
+        }
+
+        const { changes, colorHex } = processReleaseNotes(releaseNotes);
         console.log('Changes:', changes);
         console.log('ColorHex:', colorHex);
 
@@ -22,26 +25,17 @@ async function run() {
     }
 }
 
-function getChangelogFromEvent() {
+function getReleaseNotesFromEvent() {
     const eventPayloadPath = process.env.GITHUB_EVENT_PATH;
     const payload = JSON.parse(readFileSync(eventPayloadPath, 'utf8'));
-    return payload;
+    return payload.release.body;
 }
 
-function processChangelog(changelog) {
-    const { plainTextChanges, colorHex } = parseMarkdownChangelog(changelog);
-    return { changes: plainTextChanges, colorHex };
-}
-
-function parseMarkdownChangelog(changelog) {
-    // Convert Markdown syntax to Slack message formatting specifically for "Changes" section
-    let changes = changelog.replace(/^([-*])\s*/gm, '• '); // Replace list items with bullet points
-    changes = changes.replace(/\*\*(.*?)\*\*/g, '*$1*'); // Convert bold
-    changes = changes.replace(/`(.*?)`/g, '`$1`'); // Convert inline code
-    changes = changes.replace(/~~(.*?)~~/g, '~$1~'); // Convert strikethrough
-    changes = changes.replace(/\[(.*?)\]\((.*?)\)/g, '<$2|$1>'); // Convert links
+function processReleaseNotes(releaseNotes) {
+    // Here you can add specific Markdown parsing logic if needed
+    // For simplicity, we'll just return the release notes as plain text
     const colorHex = generateHexColor();
-    return { plainTextChanges: changes, colorHex };
+    return { changes: releaseNotes, colorHex };
 }
 
 function generateHexColor() {
